94a6c01b53ad7132e755553a7e6c2c064df22521
aminulislamsohag <38567911+aminulislamsohag@users.noreply.github.com>
2022-09-20 23:36:54 +0600

    Add files via upload

---
+++ CustomerManager/src/net/codejava/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package net.codejava.config;
+
+//Spring MVC configuration class here: step --03 --------->>>>
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("net.codejava")
+public class WebMvcConfig {
+	@Bean(name = "viewResolver")
+	public InternalResourceViewResolver getViewResolver() {
+	    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+	    viewResolver.setPrefix("/WEB-INF/views/");
+	    viewResolver.setSuffix(".jsp");
+	    return viewResolver;
+	}
+}
